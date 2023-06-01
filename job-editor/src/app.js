eb9503d45c0cbc045c48212130d3bd8aa955ebc0
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-06-01 11:39:09 +0300

    feat(job-editor): adds eslint

--- job-editor/src/app.js
+++ job-editor/src/app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const App = ({ onClick }) => {
   const onClickHandler = () => {
@@ -20,6 +21,10 @@ const App = ({ onClick }) => {
       </div>
     </div>
   );
+};
+
+App.propTypes = {
+  onClick: PropTypes.func,
 };
 
 export default App;
